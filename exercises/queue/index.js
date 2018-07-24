f7c18ab26a238f6d3fe6586b485c2881d8966a38
Archer Hobson <supremeoverlord@Archers-MacBook-Pro.local>
2018-07-24 18:54:42 -0500

    using queue data structure to move data iwthin array

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,20 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+    constructor() {
+        this.data = [];
+    }
+
+    add(record) {
+        //adds to front of array
+        this.data.unshift(record);
+    }
+
+    remove() {
+        //removes from end
+        return this.data.pop();
+    }
+}
 
 module.exports = Queue;
