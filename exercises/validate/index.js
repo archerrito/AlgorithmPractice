0a223b0af5c0b8b869daf2ad5582b287399646d2
Archer Hobson <supremeoverlord@Archers-MacBook-Pro.local>
2018-07-26 11:25:10 -0500

    validate binary search tree

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,30 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+//called on root node, no min/max set
+function validate(node, min = null, max = null) {
+    //not set max bound, have max value, but outside current node max value
+    //skipped initially, no max/min values
+    if (max !== null && node.data > max) {
+        return false;
+    }
+
+    if (min !== null && node.data < min) {
+        return false;
+    }
+
+    //if notde on left and calling validate returns false
+    //cause recursion to occur
+    //node on left hand side, yes, call recursion with values
+    if (node.left && !validate(node.left, min, node.data)) {
+        return false;
+    }
+
+    if (node.right && !validate(node.right, node.data, max)) {
+        return false;
+    }
+
+    return true;
+}
 
 module.exports = validate;
